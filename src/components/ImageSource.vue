df6f923bf89916d4c78a3794ed07db93a3dd50bd
Guilherme Moura <mouraggui@gmail.com>
2022-11-22 11:21:18 -0300

    feat: ImageSource component

---
+++ src/components/ImageSource.vue
@@ -0,0 +1,35 @@
+<template>
+  <picture>
+    <source
+      v-for="(source, index) in image.sources"
+      :key="index"
+      :type="source.type"
+      :srcset="source.srcset"
+      :media="source.media"
+    />
+
+    <img
+      width="100%"
+      height="100%"
+      :src="image.src"
+      :alt="image.alt"
+      :style="{ objectFit: 'contain' }"
+    />
+  </picture>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: Object,
+
+      default: () => ({
+        src: '',
+        alt: '',
+        sources: [],
+      }),
+    },
+  },
+};
+</script>
